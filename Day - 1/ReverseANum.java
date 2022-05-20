eb98d15d995ed5bd908f9a712f6a9670608c749e
Abdul Hakkeem P A <92361680+hakkeempa@users.noreply.github.com>
2022-05-20 22:57:35 +0530

    string-conversion

--- Day - 1/ReverseANum.java
+++ Day - 1/ReverseANum.java
@@ -1,22 +1,11 @@
 import java.util.Scanner;
 
-
 public class ReverseANum {
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Number to be Reversed");
-		int num = sc.nextInt();
-		int numCopy = num;
-		int length = 0;
-		while(numCopy>0){
-			length++;
-			numCopy = numCopy/10;
-		}
-		System.out.println("Orginal Num : "+num);
-		System.out.print("Reversed Num : ");
-		for (int i = 0; i < length; i++) {
-			System.out.print(num%10);
-			num = num/10;
+		String Sample = sc.nextLine();
+		for (int i = Sample.length()-1; i>=0; i--) {
+			System.out.print(Sample.charAt(i));
 		}
 	}
 }
