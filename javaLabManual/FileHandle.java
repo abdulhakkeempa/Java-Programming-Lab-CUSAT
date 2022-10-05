1330bdcd9bdc782ede2de9ba152882376f58f9f1
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-10-05 17:41:46 +0530

    file handling

---
+++ javaLabManual/FileHandle.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.util.Scanner; 
+import java.io.FileWriter; 
+import java.io.IOException; 
+import java.io.BufferedWriter; 
+import java.util.*;  
+
+
+public class FileHandle{
+    public static void main(String s[]){
+        try {
+            List<String> files=new ArrayList<String>();  
+            files.add("file1.txt");
+            files.add("file2.txt");
+
+            for(String file:files){  
+                File Obj = new File("data/"+file);
+                Scanner Reader = new Scanner(Obj);
+                while (Reader.hasNextLine()) {
+                    String data = Reader.nextLine();
+                    try{
+                        FileWriter Writer = new FileWriter("data/merged.txt",true);
+                        BufferedWriter bw = new BufferedWriter(Writer);
+                        bw.write(data+"\n");
+                        bw.close();
+                    }           
+                    catch (IOException e){
+                        e.printStackTrace(); 
+                    }    
+                }
+                Reader.close();
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }        
+    }
+}
